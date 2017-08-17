7d5c0216b1a50c04471f7771811d0f0b59b36584
Jan Klare <j.klare@cloudbau.de>
2017-08-17 14:19:40 +0200

    starting pike development patch
    Change-Id: I5955ae670fd4a87a7800203b7a9ba942bd7cc8eb

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'openstack-chef'
 maintainer_email 'openstack-dev@lists.openstack.org'
 license          'Apache 2.0'
 description      'Provides the shared database configuration for Chef for OpenStack.'
-version          '15.0.0'
+version          '16.0.0'
 
 recipe 'client', 'Installs client packages for the database used by the deployment.'
 recipe 'server', 'Installs and configures server packages for the database used by the deployment.'
@@ -19,7 +19,7 @@ recipe 'openstack-db', 'Creates necessary tables, users, and grants for OpenStac
   supports os
 end
 
-depends 'openstack-common', '>= 15.0.0'
+depends 'openstack-common', '>= 16.0.0'
 
 depends 'database', '~> 6.1'
 depends 'mariadb', '~> 1.5'
