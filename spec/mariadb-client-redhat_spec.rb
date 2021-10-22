5e26af7d046e484dc3729d6a8d9173b304992b87
Lance Albertson <lance@osuosl.org>
2021-10-22 09:59:04 -0700

    CentOS 8 support
    - Update ChefSpec
    
    Depends-On: https://review.opendev.org/c/openstack/openstack-chef/+/814234
    Depends-On: https://review.opendev.org/c/openstack/cookbook-openstack-common/+/814235
    Change-Id: I832a910d9054c2ddfde28ed2522efe7c0f414588
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- spec/mariadb-client-redhat_spec.rb
+++ spec/mariadb-client-redhat_spec.rb
@@ -1,18 +1,27 @@
 require_relative 'spec_helper'
 
 describe 'openstack-ops-database::mariadb-client' do
-  describe 'redhat' do
-    let(:runner) { ChefSpec::SoloRunner.new(REDHAT_OPTS) }
-    let(:node) do
-      runner.node
-    end
-    cached(:chef_run) do
-      runner.node.override['openstack']['db']['service_type'] = 'mariadb'
-      runner.converge(described_recipe)
-    end
+  ALL_RHEL.each do |p|
+    context "redhat #{p[:version]}" do
+      let(:runner) { ChefSpec::SoloRunner.new(p) }
+      let(:node) do
+        runner.node
+      end
+      cached(:chef_run) do
+        runner.node.override['openstack']['db']['service_type'] = 'mariadb'
+        runner.converge(described_recipe)
+      end
 
-    it 'installs mariadb python client packages' do
-      expect(chef_run).to install_package('MySQL-python')
+      case p
+      when REDHAT_7
+        it do
+          expect(chef_run).to install_package('MySQL-python')
+        end
+      when REDHAT_8
+        it do
+          expect(chef_run).to install_package('python3-PyMySQL')
+        end
+      end
     end
   end
 end
